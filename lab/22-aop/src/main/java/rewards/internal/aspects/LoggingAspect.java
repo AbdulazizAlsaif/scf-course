f48b8971a22cde395ee561eb522060d6ce3a955e
Abdulaziz Alsaif <aalsaif25@gmail.com>
2023-09-03 01:48:25 +0300

    6-AOP-programming

--- lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
+++ lab/22-aop/src/main/java/rewards/internal/aspects/LoggingAspect.java
@@ -3,9 +3,14 @@ package rewards.internal.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.stereotype.Component;
 import rewards.internal.monitor.Monitor;
 import rewards.internal.monitor.MonitorFactory;
 
@@ -17,6 +22,8 @@ import rewards.internal.monitor.MonitorFactory;
 //    where `MonitorFactory` dependency is being injected.
 //    (It is optional since there is only a single constructor in the class.)
 
+@Aspect
+@Component
 public class LoggingAspect {
     public final static String BEFORE = "'Before'";
     public final static String AROUND = "'Around'";
@@ -31,11 +38,18 @@ public class LoggingAspect {
 	}
 
 
-	// TODO-03: Write Pointcut Expression
-	// - Decide which advice type is most appropriate
-	// - Write a pointcut expression that selects only find* methods on
-	//    our repository classes.
+	@Pointcut("execution(* rewards.internal.*.*Repository.find*(..))")
+	void repoFindMethod(){
 
+	}
+
+	@Pointcut("execution(* rewards.internal.*.*Repository.update*(..))")
+	void repoUpdateMethod(){
+
+	}
+
+
+	@Before("repoFindMethod()")
 	public void implLogging(JoinPoint joinPoint) {
 		// Do not modify this log message or the test will fail
 		logger.info(BEFORE + " advice implementation - " + joinPoint.getTarget().getClass() + //
@@ -49,6 +63,7 @@ public class LoggingAspect {
 	// - Write a pointcut expression to match on all update* methods
 	//	 on all Repository classes.
 
+	@Around("repoUpdateMethod()")
 	public Object monitor(ProceedingJoinPoint repositoryMethod) throws Throwable {
 		String name = createJoinPointTraceName(repositoryMethod);
 		Monitor monitor = monitorFactory.start(name);
@@ -59,8 +74,7 @@ public class LoggingAspect {
 			//  - Be sure to return the target method's return value to the caller
 			//    and delete the line below.
 
-			return new String("Delete this line after completing TODO-08");
-
+			return  repositoryMethod.proceed();
 		} finally {
 			monitor.stop();
 			// Do not modify this log message or the test will fail
