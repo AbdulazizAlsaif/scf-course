3af28d103772f9b90a6e809aca1fc00099ba88ec
Abdulaziz Alsaif <aalsaif25@gmail.com>
2023-09-08 21:12:03 +0300

    (4)3-Spring Boot - Spring Data JPA

--- lab/34-spring-data-jpa/src/test/java/rewards/internal/StubAccountRepository.java
+++ lab/34-spring-data-jpa/src/test/java/rewards/internal/StubAccountRepository.java
@@ -2,6 +2,7 @@ package rewards.internal;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.orm.ObjectRetrievalFailureException;
 
@@ -13,7 +14,7 @@ import common.money.Percentage;
 /**
  * A dummy account repository implementation. Has a single Account "Keith and Keri Donald" with two beneficiaries
  * "Annabelle" (50% allocation) and "Corgan" (50% allocation) associated with credit card "1234123412341234".
- * 
+ *
  * Stubs facilitate unit testing. An object needing an AccountRepository can work with this stub and not have to bring
  * in expensive and/or complex dependencies such as a Database. Simple unit tests can then verify object behavior by
  * considering the state of this stub.
@@ -29,7 +30,7 @@ public class StubAccountRepository implements AccountRepository {
 		accountsByCreditCard.put("1234123412341234", account);
 	}
 
-	public Account findByCreditCard(String creditCardNumber) {
+	public Account findByCreditCardNumber(String creditCardNumber) {
 		Account account = accountsByCreditCard.get(creditCardNumber);
 		if (account == null) {
 			throw new ObjectRetrievalFailureException(Account.class, creditCardNumber);
@@ -39,5 +40,65 @@ public class StubAccountRepository implements AccountRepository {
 
 	public void updateBeneficiaries(Account account) {
 		// nothing to do, everything is in memory
+	}
+
+	@Override
+	public <S extends Account> S save(S entity) {
+		return null;
+	}
+
+	@Override
+	public <S extends Account> Iterable<S> saveAll(Iterable<S> entities) {
+		return null;
+	}
+
+	@Override
+	public Optional<Account> findById(Long aLong) {
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean existsById(Long aLong) {
+		return false;
+	}
+
+	@Override
+	public Iterable<Account> findAll() {
+		return null;
+	}
+
+	@Override
+	public Iterable<Account> findAllById(Iterable<Long> longs) {
+		return null;
+	}
+
+	@Override
+	public long count() {
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long aLong) {
+
+	}
+
+	@Override
+	public void delete(Account entity) {
+
+	}
+
+	@Override
+	public void deleteAllById(Iterable<? extends Long> longs) {
+
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Account> entities) {
+
+	}
+
+	@Override
+	public void deleteAll() {
+
 	}
 }
--- lab/34-spring-data-jpa/src/test/java/rewards/internal/StubRestaurantRepository.java
+++ lab/34-spring-data-jpa/src/test/java/rewards/internal/StubRestaurantRepository.java
@@ -2,6 +2,7 @@ package rewards.internal;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.orm.ObjectRetrievalFailureException;
 
@@ -16,7 +17,7 @@ import common.money.Percentage;
 /**
  * A dummy restaurant repository implementation. Has a single restaurant "Apple Bees" with a 8% benefit availability
  * percentage that's always available.
- * 
+ *
  * Stubs facilitate unit testing. An object needing a RestaurantRepository can work with this stub and not have to bring
  * in expensive and/or complex dependencies such as a Database. Simple unit tests can then verify object behavior by
  * considering the state of this stub.
@@ -32,12 +33,72 @@ public class StubRestaurantRepository implements RestaurantRepository {
 		restaurantsByMerchantNumber.put(restaurant.getNumber(), restaurant);
 	}
 
-	public Restaurant findByMerchantNumber(String merchantNumber) {
+	public Restaurant findByNumber(String merchantNumber) {
 		Restaurant restaurant = (Restaurant) restaurantsByMerchantNumber.get(merchantNumber);
 		if (restaurant == null) {
 			throw new ObjectRetrievalFailureException(Restaurant.class, merchantNumber);
 		}
 		return restaurant;
+	}
+
+	@Override
+	public <S extends Restaurant> S save(S entity) {
+		return null;
+	}
+
+	@Override
+	public <S extends Restaurant> Iterable<S> saveAll(Iterable<S> entities) {
+		return null;
+	}
+
+	@Override
+	public Optional<Restaurant> findById(Long aLong) {
+		return Optional.empty();
+	}
+
+	@Override
+	public boolean existsById(Long aLong) {
+		return false;
+	}
+
+	@Override
+	public Iterable<Restaurant> findAll() {
+		return null;
+	}
+
+	@Override
+	public Iterable<Restaurant> findAllById(Iterable<Long> longs) {
+		return null;
+	}
+
+	@Override
+	public long count() {
+		return 0;
+	}
+
+	@Override
+	public void deleteById(Long aLong) {
+
+	}
+
+	@Override
+	public void delete(Restaurant entity) {
+
+	}
+
+	@Override
+	public void deleteAllById(Iterable<? extends Long> longs) {
+
+	}
+
+	@Override
+	public void deleteAll(Iterable<? extends Restaurant> entities) {
+
+	}
+
+	@Override
+	public void deleteAll() {
+
 	}
 
 	/**
