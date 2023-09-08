f29e7b02badcf6c626d064ccefff9269f0f90e58
Abdulaziz Alsaif <aalsaif25@gmail.com>
2023-09-08 19:20:01 +0300

    (4)1-Spring Boot Feature Introduction

---
+++ lab/30-jdbc-boot/src/test/java/rewards/JdbcBootApplicationTests.java
@@ -0,0 +1,21 @@
+package rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class JdbcBootApplicationTests {
+	public static final String QUERY = "SELECT count(*) FROM T_ACCOUNT";
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	void testNumberOfAccount(){
+		Long numOfAccount=jdbcTemplate.queryForObject(QUERY,Long.class);
+
+		assertEquals(21,numOfAccount);
+	}
+
+}
